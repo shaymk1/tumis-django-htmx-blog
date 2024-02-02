41f2c212184b2f2809ea0bbb09d85659f9ae0bda
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-02 13:26:11 +0200

    moved the template folder into the core from the root, created blog folder inside the template folder, within it ,created an index.html. Configured both urls, added a function in the views for home-page

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def home_page(request):
+    model = Post
+    posts = Post.objects.all().order_by("-created_at")
+    context = {"posts": posts, "model": model}
+    return render(request, "blog/index.html", context)
