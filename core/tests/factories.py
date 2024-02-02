f19c2ca84bf35973d2983bb8555daf8ef4b7eee3
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-02 10:32:59 +0200

    modified factories.py and moved pytest.ini into the root directory to pytest configuration issues. Pytest ran with with 2 passed and 1 warning

--- core/tests/factories.py
+++ core/tests/factories.py
@@ -1,5 +1,6 @@
 import factory
 from django.contrib.auth.models import User
+
 from blog.models import Post
 
 
@@ -7,19 +8,27 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-        password = "test"
-        usernamr = "test"
-        is_superuser = True
-        is_staff = True
+    password = "test"
+    username = "test"
+    is_superuser = True
+    is_staff = True
 
 
 class PostFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Post
 
-        title = "x"
-        subtitle = "x"
-        slug = "x"
-        author = factory.SubFactory(User)
-        content = "x"
-        status = "published"
+    title = "x"
+    subtitle = "x"
+    slug = "x"
+    author = factory.SubFactory(UserFactory)
+    content = "x"
+    status = "published"
+
+    @factory.post_generation
+    def tags(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            self.tags.add(*extracted)
