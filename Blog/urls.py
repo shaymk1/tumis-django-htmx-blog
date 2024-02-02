ca926e2f921af5593f4b117e476853a64dd60a18
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2024-02-02 20:44:34 +0200

    created base.html,and extended it to article.html, articles.html, index.html. Wrote functions for templates, modified a .gitignore

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name="homepage"),
+    path("", views.index, name="index"),
+    path("article", views.article, name="article"),
+    path("articles", views.articles, name="articles")
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from .models import Post
 
 
-def home_page(request):
+def index(request):
     model = Post
     posts = Post.objects.all().order_by("-created_at")
     context = {"posts": posts, "model": model}
     return render(request, "blog/index.html", context)
+
+
+def article(request):
+    return render(request, "blog/article.html")
+
+
+def articles(request):
+    return render(request, "blog/articles.html")
